ddc82a736242fbab73858f68334c2eb5701a568c
simyeongryu <rsm0503@gmail.com>
2020-05-19 13:54:12 +0900

    2. Hello World with Styled Components

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Styled Components
 
+별도의 sass 파일이나 css 파일 없이 css 작업을 할 수 있고 components를 좀 더 캡슐화한다.
+
+## 설치
+
+```
+$ yarn add styled-components
+```
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #ff7979;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  /* styled-components는 컴포넌트이기 때문에 
+   * props를 사용할 수 있다.
+  **/
+  background-color: ${props => (props.danger ? "#c0392b" : "#2ecc71")};
+`;
 
 const App = () => {
   return (
-    <>
-      <button className="button button--success">Hello</button>
-      <button className="button button--danger">Hello</button>
-    </>
+    <Container>
+      <Button danger={false}>Hello</Button>
+      <Button danger={true}>Hello</Button>
+    </Container>
   );
 };
 
